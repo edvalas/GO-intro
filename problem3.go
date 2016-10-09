08dc28bcdb1552bfa42a00adbf097fbcda628234
edvalas <edvalas@users.noreply.github.com>
2016-10-09 13:29:43 +0100

    Update problem3.go

--- problem3.go
+++ problem3.go
@@ -1,7 +1,7 @@
 package main
 
 import "fmt"
-
+//this problem takes in input from keyboard from the user and reverses the input string
 func Reverse(s string) string {
     runes := []rune(s)
     for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
